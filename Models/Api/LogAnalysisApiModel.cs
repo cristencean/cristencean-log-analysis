6cca5e4b8ad50bf6ddacbe90fb318e93bbee22ae
Cristian Balan <cristian.balan@shoutdigital.com>
2025-05-13 17:39:50 +0100

    Log analyzer app implementation

---
+++ Models/Api/LogAnalysisApiModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LogAnalysis.Models.Api
+{
+    public class LogAnalysisApiModel
+    {
+        [JsonPropertyName("userActivity")]
+        public List<UserActivityModel> UserActivity { get; set; } = new List<UserActivityModel>();
+
+        [JsonPropertyName("uniqueUsers")]
+        public int UniqueUsers { get; set; } = 0;
+
+        [JsonPropertyName("errors")]
+        public ErrorTypeCountModel Errors { get; set; } = new ErrorTypeCountModel();
+    }
+}
---
+++ Models/UserActivityModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LogAnalysis.Models
+{
+    public class UserActivityModel
+    {
+
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+
+        [JsonPropertyName("event")]
+        public string Event { get; set; } = string.Empty;
+
+        [JsonPropertyName("timestamp")]
+        public string Timestamp { get; set; } = string.Empty;
+    }
+}
